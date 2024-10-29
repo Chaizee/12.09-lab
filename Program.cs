f19333b032d9b6bf45f84966e6adadcbb48a0525
Chaizee <zuzindima26@gmail.com>
2024-10-29 11:21:54 +0600

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,16 @@
+﻿class Lab
+{
+    public static void Main(string[] args)
+    {
+        Console.WriteLine("Введите значение переменной a");
+        int a = int.Parse(Console.ReadLine());
+        Console.WriteLine("Введите значение переменной b");
+        int b = int.Parse(Console.ReadLine());
+        a = a + b;
+        b = a - b;
+        a = a - b;
+
+        Console.WriteLine($"a={a}, b={b}");
+    }
+    
+}
